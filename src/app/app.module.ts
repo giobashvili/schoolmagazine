728f9eb62ff224b717d845d307df0bfa5aa20301
giobashvili <58891718+giobashvili@users.noreply.github.com>
2020-01-08 00:32:41 +0400

    add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {ServiceAPIService} from './service/service-api.service';
 import {HttpClientModule} from '@angular/common/http';
+import { FooterComponent } from './footer/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
