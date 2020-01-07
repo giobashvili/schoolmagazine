48f3089148a53cc2bc1bb961f9f86b73c6d04007
giobashvili <58891718+giobashvili@users.noreply.github.com>
2020-01-08 00:42:24 +0400

    add page/auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import {ServiceAPIService} from './service/service-api.service';
 import {HttpClientModule} from '@angular/common/http';
 import { FooterComponent } from './footer/footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { AuthComponent } from './page/auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
